7fa15b4aa8bf356cadf2ba51519f4e4411d412a8
Debora Orsolich <debora.orsolich@cognisess.com>
2022-05-17 17:13:31 +0100

    t5 model 0.2 Designing 4 experiments based on preprocessing settings
    Hotfix:
    - Added a parameter that let you choose 1/4 preprocessed options
    - Spited encoder in encoder.py and trainer.py

--- qg/t5_model/objects/preprocessor.py
+++ qg/t5_model/objects/preprocessor.py
@@ -1,6 +1,7 @@
 
 from qg.config.config import get_logger
 _logger = get_logger(logger_name=__name__)
+
 class DataProcessor:
     def __init__(self, sep_token, eos_token, setting = "") -> None:
         self.sep_token = sep_token
@@ -11,20 +12,27 @@ class DataProcessor:
         self.prev_questions = ""
         self.index = 0
         self.relevant_examples_indices = []
+        self.init_indices = []
+        self.len_answers = 0
 
     def process(self, dataset):
 
-        if self.setting == "AQPL":
+        # Experiment 4: one question per line
+        if self.setting == "OQPL": 
+            self.check_dataset = dataset
+            dataset = dataset.map(self._one_question_per_line)
+        
+        # Experiment 3: all questions per line
+        elif self.setting == "AQPL": # all questions per line
             dataset = dataset.map(self._all_questions_per_line)
 
-        elif self.setting == "OQPL":
-            dataset = dataset.map(self._one_question_per_line)
-
-        elif self.setting == "AA": # answer awareness
+        # Experiment 2: answer awareness
+        elif self.setting == "AA":
             dataset = dataset.map(self._answer_awareness)
 
+        # Experiment 1: basic processing
         dataset = dataset.map(self._eos_token)
-        _logger.info(self.relevant_examples_indices)
+
         return dataset
 
     def _eos_token(self, example):
@@ -37,11 +45,12 @@ class DataProcessor:
         return example
 
     def _all_questions_per_line(self, example):
+
         if example["context"] == self.prev_context:
             self.prev_questions += example["question"] + self.sep_token
             example["question"] = self.prev_questions
             self.prev_context = example["context"]
-            self.index += 1
+            self.index = self.index + 1
 
         else:
             self.prev_questions = example["question"] + self.sep_token
@@ -53,20 +62,50 @@ class DataProcessor:
         return example
 
     def _answer_awareness(self, example):
-        init_answer_token = " [ANSS] "
-        end_answer_token = " [ANSE] "
+        init_answer_token = "[ANSS] "
+        end_answer_token = " [ANSE]"
         
-        i_init = example["answers"]["answer_start"][0]
-        i_end = i_init + len(init_answer_token) + len(example["answers"]["text"])
+        # Examples that don't contain answers
+        if example["answers"]["answer_start"] == []:
+            return example
+        else:
+            # Examples might contain more that one answer
+            for i in range(len(example["answers"]["answer_start"])):
+                
+                if i == 0:
+                    i_init = example["answers"]["answer_start"][i]
+                else:
+                    i_init = example["answers"]["answer_start"][i] + (len(init_answer_token)*i)
 
-        example["context"] = example["context"][:i_init] + init_answer_token + example[i_init:]
-        example["context"] = example["context"][:i_end] + end_answer_token + example[i_end:]
-        return example
+                str_context = str(example["context"])
+                example["context"] = str_context[:i_init] + init_answer_token + str_context[i_init:]
+                self.init_indices.append(i_init)
+
+            for i in range(len(example["answers"]["answer_start"])):
+
+                if i == 0:
+                    i_end = self.init_indices[i] + len(init_answer_token) + len(example["answers"]["text"][0])
+                    self.len_answers += len(example["answers"]["text"][0])
+                else:
+                    i_end = self.init_indices[i] + len(init_answer_token)*i+1 + self.len_answers
+
+                str_context = str(example["context"])
+                example["context"] = str_context[:i_end] + end_answer_token + str_context[i_end:]
+
+            return example
 
     def filter_examples(self, processed_train_data):
-        length = len(processed_train_data)
+
+        # only applicable in setting AQPL
         if self.setting == "AQPL":
-                processed_train_data = processed_train_data.select(self.relevant_examples_indices)
-                _logger.info(f"""Filtered in {len(self.relevant_examples_indices)}, 
-                filtered out {length - len(self.relevant_examples_indices)}""")
+
+            # due to cache memory, self.relevant_examples_indices can be an empty list
+            # this if statement checks it and populates it if needed
+            if self.relevant_examples_indices == []:
+                self.check_dataset = self.check_dataset.map(self._one_question_per_line)
+            
+            processed_train_data = processed_train_data.select(self.relevant_examples_indices)
+            _logger.info(f"""Filtered in {len(self.relevant_examples_indices)}, 
+            filtered out {len(processed_train_data) - len(self.relevant_examples_indices)}""")
+            
         return processed_train_data
